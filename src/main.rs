8093271ff46208215002b985686844029dcec047
notoriaga <smeyer@fastmail.com>
2023-01-14 18:43:54 -0800

    support file/stdin input

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,256 @@
-use std::boxed::Box;
 use std::cell::RefCell;
-use std::error::Error;
-use std::io::{self, Write};
-use std::ops::Add;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::fmt::Write;
+use std::io::{self, Write as _};
+use std::iter;
+use std::path::PathBuf;
+use std::sync::{
+    atomic::{AtomicBool, Ordering::SeqCst},
+    mpsc, Arc,
+};
+use std::thread;
+use std::time::{Duration, SystemTime};
 
 use chrono::{DateTime, Utc};
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 use curl::easy::{Easy, HttpVersion, List, ReadError};
 
-#[derive(Debug, Parser)]
-#[clap(name = "ntripping", about = "NTRIP command line client.", version = env!("VERGEN_SEMVER_LIGHTWEIGHT"))]
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+thread_local! {
+    static CURL: RefCell<Easy> = RefCell::new(Easy::new());
+}
+
+#[derive(Debug, Clone, Parser)]
+#[command(
+    name = "ntripping",
+    about = "NTRIP command line client",
+    version = env!("VERGEN_SEMVER_LIGHTWEIGHT"),
+    group(
+        ArgGroup::new("gga")
+            .conflicts_with_all(["input", "cra"])
+            .args(["lat", "lon", "height"])
+            .multiple(true),
+    ),
+    group(
+        ArgGroup::new("cra")
+            .conflicts_with_all(["input", "gga"])
+            .args(["request_counter", "area_id", "corrections_mask", "solution_id"])
+            .multiple(true),
+    ),
+)]
 struct Cli {
     /// URL of the NTRIP caster
-    #[clap(long, default_value = "na.skylark.swiftnav.com:2101/CRS")]
+    #[arg(long, default_value = "na.skylark.swiftnav.com:2101/CRS")]
     url: String,
 
     /// Receiver latitude to report, in degrees
-    #[clap(long, default_value = "37.77101999622968", allow_hyphen_values = true)]
-    lat: String,
+    #[arg(long, default_value_t = 37.77101999622968, allow_hyphen_values = true)]
+    lat: f64,
 
     /// Receiver longitude to report, in degrees
-    #[clap(
-        long,
-        default_value = "-122.40315159140708",
-        allow_hyphen_values = true
-    )]
-    lon: String,
+    #[arg(long, default_value_t = -122.40315159140708, allow_hyphen_values = true)]
+    lon: f64,
 
     /// Receiver height to report, in meters
-    #[clap(long, default_value = "-5.549358852471994", allow_hyphen_values = true)]
-    height: String,
+    #[arg(long, default_value_t = -5.549358852471994, allow_hyphen_values = true)]
+    height: f64,
 
     /// Client ID
-    #[clap(
+    #[arg(
         long,
         default_value = "00000000-0000-0000-0000-000000000000",
         alias = "client"
     )]
     client_id: String,
 
-    #[clap(short, long)]
+    #[arg(short, long)]
     verbose: bool,
 
     /// Receiver time to report, as a Unix time
-    #[clap(long)]
+    #[arg(long)]
     epoch: Option<u32>,
 
     /// Username credentials
-    #[clap(long)]
+    #[arg(long)]
     username: Option<String>,
 
     /// Password credentials
-    #[clap(long)]
+    #[arg(long)]
     password: Option<String>,
 
     /// GGA update period, in seconds. 0 means to never send a GGA
-    #[clap(long, default_value = "10")]
+    #[arg(long, default_value_t = 10)]
     gga_period: u64,
 
     /// Request counter allows correlation between message sent and acknowledgment response from corrections stream
-    #[clap(long)]
+    #[arg(long)]
     request_counter: Option<u8>,
 
     /// Area ID to be used in generation of CRA message. If this flag is set, ntripping outputs messages of type CRA rather than the default GGA
-    #[clap(long)]
+    #[arg(long)]
     area_id: Option<u32>,
 
     /// Field specifying which types of corrections are to be received
-    #[clap(long)]
+    #[arg(long)]
     corrections_mask: Option<u16>,
 
     /// Solution ID, the identifier of the connection stream to reconnect to in the event of disconnections
-    #[clap(long)]
+    #[arg(long)]
     solution_id: Option<u8>,
+
+    /// Path to a YAML file containing a list of messages to send to the caster
+    #[arg(long)]
+    input: Option<PathBuf>,
 }
 
-type Result<T> = std::result::Result<T, Box<dyn Error>>;
+#[derive(Debug, Clone, Copy, serde::Deserialize)]
+struct Command {
+    epoch: Option<u32>,
+    after: Option<u64>,
+    crc: Option<u8>,
+    #[serde(flatten)]
+    message: Message,
+}
 
-thread_local! {
-    static CURL: RefCell<Easy> = RefCell::new(Easy::new());
-    static LAST: RefCell<SystemTime> = RefCell::new(UNIX_EPOCH);
+impl Command {
+    fn to_bytes(&self) -> Vec<u8> {
+        let now = self.epoch.map_or_else(SystemTime::now, |e| {
+            SystemTime::UNIX_EPOCH + Duration::from_secs(e.into())
+        });
+        let message = self.message.format(now.into());
+        let checksum = self.crc.unwrap_or_else(|| checksum(message.as_bytes()));
+        let message = format!("{message}*{checksum:X}\r\n");
+        message.into_bytes()
+    }
+}
+
+#[derive(Debug, Clone, Copy, serde::Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Message {
+    Gga {
+        lat: f64,
+        lon: f64,
+        height: f64,
+    },
+    Cra {
+        request_counter: Option<u8>,
+        area_id: Option<u32>,
+        corrections_mask: Option<u16>,
+        solution_id: Option<u8>,
+    },
+}
+
+impl Message {
+    fn format(&self, time: DateTime<Utc>) -> String {
+        match *self {
+            Message::Gga { lat, lon, height } => {
+                let time = time.format("%H%M%S.00");
+
+                let latn = ((lat * 1e8).round() / 1e8).abs();
+                let lonn = ((lon * 1e8).round() / 1e8).abs();
+
+                let lat_deg = latn as u16;
+                let lon_deg = lonn as u16;
+
+                let lat_min = (latn - (lat_deg as f64)) * 60.0;
+                let lon_min = (lonn - (lon_deg as f64)) * 60.0;
+
+                let lat_dir = if lat < 0.0 { 'S' } else { 'N' };
+                let lon_dir = if lon < 0.0 { 'W' } else { 'E' };
+
+                format!(
+                    "$GPGGA,{},{:02}{:010.7},{},{:03}{:010.7},{},4,12,1.3,{:.2},M,0.0,M,1.7,0078",
+                    time, lat_deg, lat_min, lat_dir, lon_deg, lon_min, lon_dir, height
+                )
+            }
+            Message::Cra {
+                request_counter,
+                area_id,
+                corrections_mask,
+                solution_id,
+            } => {
+                let mut s = String::from("$PSWTCRA,");
+                if let Some(request_counter) = request_counter {
+                    write!(&mut s, "{request_counter}").unwrap();
+                }
+                s.push(',');
+                if let Some(area_id) = area_id {
+                    write!(&mut s, "{area_id}").unwrap();
+                }
+                s.push(',');
+                if let Some(corrections_mask) = corrections_mask {
+                    write!(&mut s, "{corrections_mask}").unwrap();
+                }
+                s.push(',');
+                if let Some(solution_id) = solution_id {
+                    write!(&mut s, "{solution_id}").unwrap();
+                }
+                s
+            }
+        }
+    }
+}
+
+fn get_commands(opt: &Cli) -> Result<Box<dyn Iterator<Item = Command> + Send>> {
+    if opt.gga_period == 0 && opt.input.is_none() {
+        return Ok(Box::new(iter::empty()));
+    }
+    match opt.clone() {
+        Cli {
+            input: Some(path), ..
+        } => {
+            let file = std::fs::File::open(path)?;
+            let cmds: Vec<_> = serde_yaml::from_reader(file)?;
+            Ok(Box::new(cmds.into_iter()))
+        }
+        opt if opt.area_id.is_some() => {
+            let first = Command {
+                epoch: opt.epoch,
+                after: None,
+                crc: None,
+                message: Message::Cra {
+                    request_counter: opt.request_counter,
+                    area_id: opt.area_id,
+                    corrections_mask: opt.corrections_mask,
+                    solution_id: opt.solution_id,
+                },
+            };
+            let it = iter::successors(Some(first), move |prev| {
+                let mut next = *prev;
+                next.after = Some(opt.gga_period);
+                if let Message::Cra {
+                    request_counter: Some(ref mut counter),
+                    ..
+                } = &mut next.message
+                {
+                    *counter = counter.wrapping_add(1);
+                }
+                Some(next)
+            });
+            Ok(Box::new(it))
+        }
+        opt => {
+            let first = Command {
+                epoch: opt.epoch,
+                after: None,
+                crc: None,
+                message: Message::Gga {
+                    lat: opt.lat,
+                    lon: opt.lon,
+                    height: opt.height,
+                },
+            };
+            let rest = iter::repeat(Command {
+                after: Some(opt.gga_period),
+                ..first
+            });
+            Ok(Box::new(iter::once(first).chain(rest)))
+        }
+    }
 }
 
 fn checksum(buf: &[u8]) -> u8 {
-    let mut sum: u8 = 0;
+    let mut sum = 0;
     for c in &buf[1..] {
         sum ^= c;
     }
@@ -94,23 +260,9 @@ fn checksum(buf: &[u8]) -> u8 {
 fn main() -> Result<()> {
     let opt = Cli::parse();
 
-    let latf: f64 = opt.lat.parse::<f64>()?;
-    let lonf: f64 = opt.lon.parse::<f64>()?;
-    let heightf: f64 = opt.height.parse::<f64>()?;
-
-    let latn = ((latf * 1e8).round() / 1e8).abs();
-    let lonn = ((lonf * 1e8).round() / 1e8).abs();
-
-    let lat_deg: u16 = latn as u16;
-    let lon_deg: u16 = lonn as u16;
-
-    let lat_min: f64 = (latn - (lat_deg as f64)) * 60.0;
-    let lon_min: f64 = (lonn - (lon_deg as f64)) * 60.0;
-
-    let lat_dir = if latf < 0.0 { 'S' } else { 'N' };
-    let lon_dir = if lonf < 0.0 { 'W' } else { 'E' };
-
-    let mut request_counter = opt.request_counter.unwrap_or(0);
+    let (tx, rx) = mpsc::sync_channel::<Vec<u8>>(1);
+    let ready = Arc::new(AtomicBool::new(true));
+    let cmds = get_commands(&opt)?;
 
     CURL.with(|curl| -> Result<()> {
         let mut curl = curl.borrow_mut();
@@ -146,60 +298,58 @@ fn main() -> Result<()> {
 
         curl.write_function(|buf| Ok(io::stdout().write_all(buf).map_or(0, |_| buf.len())))?;
 
-        curl.progress_function(|_dltot, _dlnow, _ultot, _ulnow| {
-            let now = SystemTime::now();
-            let elapsed = LAST.with(|last| {
-                let dur = now.duration_since(*last.borrow());
-                dur.unwrap_or_else(|_| Duration::from_secs(0)).as_secs()
-            });
-            if elapsed > 10 {
-                CURL.with(|curl| curl.borrow().unpause_read().unwrap());
+        curl.progress_function({
+            let ready = Arc::clone(&ready);
+            move |_dltot, _dlnow, _ultot, _ulnow| {
+                if ready.swap(false, SeqCst) {
+                    if let Err(e) = CURL.with(|curl| curl.borrow().unpause_read()) {
+                        eprintln!("unpause error: {e}");
+                        return false;
+                    }
+                }
+                true
             }
-            true
         })?;
 
         curl.read_function(move |mut buf: &mut [u8]| {
-            let now = if let Some(epoch) = opt.epoch {
-                SystemTime::UNIX_EPOCH.add(Duration::from_secs(epoch.into()))
-            } else {
-                SystemTime::now()
+            let Ok(bytes) = rx.try_recv() else {
+                return Err(ReadError::Pause);
             };
-            let elapsed = LAST.with(|last| {
-                let dur = now.duration_since(*last.borrow());
-                dur.unwrap_or_else(|_| Duration::from_secs(0)).as_secs()
-            });
-            if opt.gga_period > 0 && elapsed > opt.gga_period {
-                LAST.with(|last| *last.borrow_mut() = now);
-                let datetime: DateTime<Utc> = now.into();
-                let time = datetime.format("%H%M%S.00");
-                let message = match &opt.area_id {
-                    Some(area_id) => {
-                        let corrections_mask = &opt.corrections_mask.unwrap_or(0);
-                        let solution_id = match &opt.solution_id {
-                            Some(solution_id) => solution_id.to_string(),
-                            None => String::new()
-                        };
-                        format!("$PSWTCRA,{},{},{},{}", request_counter, area_id, corrections_mask, solution_id)
-                    },
-                    None => {
-                        format!("$GPGGA,{},{:02}{:010.7},{},{:03}{:010.7},{},4,12,1.3,{:.2},M,0.0,M,1.7,0078",
-                        time, lat_deg, lat_min, lat_dir, lon_deg, lon_min, lon_dir, heightf)
-                    }
-                };
-                request_counter = request_counter.overflowing_add(1).0;
-                let checksum = checksum(message.as_bytes());
-                let message = format!("{}*{:X}\r\n", message, checksum);
-                buf.write_all(message.as_bytes()).unwrap();
-                Ok(buf.len())
-            } else {
-                Err(ReadError::Pause)
+            if let Err(e) = buf.write_all(&bytes) {
+                eprintln!("write error: {e}");
+                return Err(ReadError::Abort);
             }
+            Ok(bytes.len())
         })?;
 
         Ok(())
     })?;
 
-    CURL.with(|curl| -> Result<()> { Ok(curl.borrow().perform()?) })?;
+    if atty::is(atty::Stream::Stdin) {
+        thread::spawn(move || {
+            for cmd in cmds {
+                if let Some(d) = cmd.after {
+                    thread::sleep(Duration::from_secs(d));
+                }
+                if tx.send(cmd.to_bytes()).is_err() {
+                    break;
+                }
+                ready.store(true, SeqCst);
+            }
+        });
+    } else {
+        thread::spawn(move || {
+            let stdin = io::stdin().lock();
+            for line in io::BufRead::lines(stdin) {
+                if tx.send(line.unwrap().into_bytes()).is_err() {
+                    break;
+                }
+                ready.store(true, SeqCst);
+            }
+        });
+    };
+
+    CURL.with(|curl| curl.borrow().perform())?;
 
     Ok(())
 }
