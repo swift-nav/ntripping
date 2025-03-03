bfbc0e1853c15868bbd9c8376db34d72189c190e
Patrick Crumley <patrick.crumley@swift-nav.com>
2025-03-03 11:27:27 -0800

    update to latest vergen-gitcl (#272)
    Removes an old vergen dependency and replaces it with the latest
    version. Sets the version in of cli tool based off the git tag at build
    time

--- build.rs
+++ build.rs
@@ -1,11 +1,20 @@
-extern crate vergen;
-
-use vergen::{generate_cargo_keys, ConstantsFlags};
+//! makes the git tag info available at build time
+use vergen_gitcl::{Emitter, GitclBuilder};
 
 fn main() {
-    let mut flags = ConstantsFlags::all();
-    flags.toggle(ConstantsFlags::SEMVER_FROM_CARGO_PKG);
+    // Make vergen `git describe` to populate some env vars at build time
+    let gitcl = GitclBuilder::default()
+        .describe(
+            true, // --dirty
+            true, // --tags [to allow lightweight tags]
+            None, // no glob matches passed in
+        )
+        .build()
+        .expect("Unable to build. Please ensure git is installed");
 
-    // Generate the 'cargo:' key output
-    generate_cargo_keys(flags).expect("Unable to generate the cargo keys!");
+    Emitter::default()
+        .add_instructions(&gitcl)
+        .expect("Unable to add git instructions")
+        .emit()
+        .expect("Unable to emit instructions");
 }
